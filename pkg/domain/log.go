bcdafc51365e6a6387956e75edb63e1991aca315
Ernur Eskermes <eskermesernur86@gmail.com>
2022-04-20 11:22:15 +0600

    change to string

--- pkg/domain/log.go
+++ pkg/domain/log.go
@@ -36,7 +36,7 @@ var (
 type LogItem struct {
 	Entity    string    `bson:"entity"`
 	Action    string    `bson:"action"`
-	EntityID  int64     `bson:"entity_id"`
+	EntityID  string    `bson:"entity_id"`
 	Timestamp time.Time `bson:"timestamp"`
 }
 
